7622cdbfcf550cc2246073532fcbdbbb9a78aab2
Heleno Cardoso <helenocardosofilho@gmail.com>
2021-03-16 21:03:30 -0300

    Department entity class

---
+++ src/application/ProgramDAO.java
@@ -0,0 +1,11 @@
+package application;
+
+import model.entities.Department;
+
+public class ProgramDAO {
+
+	public static void main(String[] args) {
+		Department dep = new Department(1, "Fiscal");
+		System.out.println(dep);
+	}
+}
